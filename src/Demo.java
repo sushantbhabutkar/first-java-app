04d078e9c9b1db197d4d49b0572ed082f0bdc0ad
sushantbhabutkar <133850473+sushantbhabutkar@users.noreply.github.com>
2023-05-18 10:23:18 +0530

    java

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		ColorPoint c1=new ColorPoint(4, 5, "Blue");
+		c1.display();
+		ColorPoint c2=new ColorPoint(6, 7, "Red");
+		c2.display();
+	}
+
+}
---
+++ src/Point.java
@@ -0,0 +1,14 @@
+
+public class Point {
+	private int x,y;
+	public Point(int x,int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	public void display()
+	{
+		System.out.println("["+x+","+y+"]");
+	}
+
+}
